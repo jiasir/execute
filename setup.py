d5a6a256b9d60843a54a5d421f4e22547c19e1fd
jiasir <jiasir@icloud.com>
2015-02-16 21:23:55 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+__author__ = 'jiasir (Taio Jia) <jiasir@icloud.com>'
+
+from setuptools import setup, find_packages
+
+setup(
+    name='execute',
+    version='0.0.1',
+    packages=find_packages(),
+    author='jiasir',
+    author_email='jiasir@icloud.com',
+    description='A python module to execute command-line',
+    license='MIT License',
+    keywords='execute command-line',
+    url='https://github.com/jiasir/execute'
+)
